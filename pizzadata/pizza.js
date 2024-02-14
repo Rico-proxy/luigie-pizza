b893a0d953f131affdb71561452d8f279681aa70
RichardOahimire <richardoahimire@gmail.com>
2024-02-14 18:18:39 +0100

    arrange

---
+++ pizzadata/pizza.js
@@ -0,0 +1,50 @@
+const pizzaData = [
+    {
+      title: "Pizza Napoletana",
+      description: "Giovanni Santarpia, a Neapolitan pizza virtuoso, enchants Tuscany with a thin, crispy masterpiece — a culinary sonnet embodying Italy's rich flavors.",
+      image: "/assets/pizza.webp",
+    },
+    {
+      title: "Pizza Tonda Romana",
+      description: "Pizza alla pala isn’t the only variety of pizza from Italy’s capital city. This kind is flat and round and it has a very thin crust.",
+      image: "/assets/pizza2.webp",
+    },
+    {
+      title: "Pizza Fritta",
+      description: "A classic Neapolitan street food, pizza fritta is fried pizza dough. It comes in many different shapes and forms.",
+      image: "/assets/pizza3.webp",
+    },
+    {
+        title: "Pizza Margherita",
+        description: "There's not much more to say when it comes to this classic Italian pizza – it is quite simply the queen of the table.",
+        image: "/assets/pizza4.jpg",
+    },
+    {
+        title: "Pizza Marinara",
+        description: " For this traditional Italian pizza, the secret lies in its simplicity. Ordering this pizza is an excellent way to test the dough and the quality of the ingredients of the pizzeria",
+        image: "/assets/pizza5.jpg",
+    },
+    {
+        title: "Pizza Prosciutto e funghi",
+        description: "Simplicity is also the name of the game with this pizza – it’s a ham and mushroom pizza, but turbo charged because it’s Italian-style.",
+        image: "/assets/pizza6.jpg",
+    },
+    {
+        title: "Pizza Quattro Stagioni",
+        description: "Another of the Italians' favourite pizzas is undoubtedly the Quattro Stagioni, which means 'Four Seasons'.",
+        image: "/assets/pizza7.jpg",
+    },
+    {
+        title: "Pizza Capricciosa",
+        description: "Very similar to the Quattro Stagioni is the classic Capricciosa pizza. The ingredients are the pretty much same.",
+        image: "/assets/pizza8.jpg",
+    },
+    {
+        title: "Pizza Quattro Formaggi",
+        description: "You have to be a true cheese lover to appreciate this creamy, delicious and attractive white pizza.",
+        image: "/assets/pizza9.png",
+    },
+  ];
+  
+  export default pizzaData;
+  
