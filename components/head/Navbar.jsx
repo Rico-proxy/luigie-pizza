d7ad0aaa98a6280a3756ccda5814ccf6984b8bcb
RichardOahimire <richardoahimire@gmail.com>
2024-02-26 16:54:39 +0100

    redo

--- components/head/Navbar.jsx
+++ components/head/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <div className='text-white z-20 h-32 flex  items-center w-full justify-around'>
       <div>
-        <h1 className='text-[20px] mx-20 md:mx-[190px] lg:mx-0  rubik w-0 lg:text-[36px] text-red-200'>LUIGIE PIZZA</h1>
+        <h1 className='text-[20px] mx-24 md:mx-[190px] lg:mx-0  rubik w-0 lg:text-[36px] text-red-200'>LUIGIE PIZZA</h1>
       </div>
       <div className='hidden lg:cursor-pointer rubik  lg:p-3 lg:flex lg:space-x-4 lg:border-2 lg:border-white lg:rounded-2xl'>
         <div className='hover:text-red-200 active:underline flex flex-row items-center'>
