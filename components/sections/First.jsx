33742e1abf8fd008cf7a92d0dd33cacac3495230
RichardOahimire <richardoahimire@gmail.com>
2024-02-26 16:43:39 +0100

    redo

--- components/sections/First.jsx
+++ components/sections/First.jsx
@@ -14,7 +14,7 @@ const First = () => {
   return (
     <div data-aos="zoom-in" data-aos-delay="100"  data-aos-easing="linear" data-aos-duration="2000" className=' min-h-screen p-16 flex items-center  justify-center text-white   pt-0 '> 
            
-            <h1 className='text-[50px] neonText  lg:w-1/2  lg:text-[100px]  rubik text-center'>
+            <h1 className='text-[60px] neonText  lg:w-1/2  lg:text-[100px]  rubik text-center'>
                <p className='leading-none'> Taste Italy Or Risk A Flavor Frenzy.</p>
             </h1>
     </div>
