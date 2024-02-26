693198d17746665671807131cd2589986d1740be
RichardOahimire <richardoahimire@gmail.com>
2024-02-26 16:39:45 +0100

    redo

--- components/sections/First.jsx
+++ components/sections/First.jsx
@@ -7,34 +7,18 @@ import { GiChefToque } from 'react-icons/gi';
 const First = () => {
   useEffect(() => {
     AOS.init({
-      duration: 500,
       
-      // Add more enchanting options as needed
+      once: true, // Whether to only animate once
     });
   }, []);
-
   return (
-    <section id='home' data-aos-anchor-placement="bottom-bottom" className="lg:min-h-screen text-[50px] h-min-screen text-pink-200 px-5 pt-20 lg:pt-0 lg:text-[66px] lg:mx-[50vh] genos lg:animate-pulse lg:p-16 font-extrabold">
-      <h1 className=" lg:flex lg:flex-row items-center lg:w-[2px] italic">
-        <span
-          data-aos="fade-right"
-       
-          data-aos-easing="ease-in-sine"
-          data-aos-delay="50"
-          className="pr-4 flex items-center"
-        >
-          Taste <GiChefToque />
-        </span>
-        <span
-          data-aos="fade-left"
-          data-aos-easing="ease-in-sine"
-          data-aos-delay="50"
-        >
-          <p>Italy or risk a flavor frenzy.</p>
-        </span>
-      </h1>
-    </section>
-  );
-};
+    <div data-aos="zoom-in" data-aos-delay="100"  data-aos-easing="linear" data-aos-duration="2000" className=' min-h-screen p-16 flex items-center  justify-center text-white   pt-0 '> 
+           
+            <h1 className='text-[80px] neonText  lg:w-1/2  lg:text-[100px]  rubik text-center'>
+               <p className='leading-none'> Taste Italy Or Risk A Flavor Frenzy.</p>
+            </h1>
+    </div>
+  )
+}
 
-export default First;
+export default First
